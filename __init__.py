d40d3cb3251b197324846ddfc3c194a4c851c585
Luciano Rossi <lukio@gcoop.coop>
2026-07-17 18:30:02 -0300

    fix: migrate to Tryton 8.0 - remove Pool.register, add [register] in tryton.cfg

--- __init__.py
+++ __init__.py
@@ -1,15 +1,2 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-
-from trytond.pool import Pool
-from . import subscription
-
-
-def register():
-    Pool.register(
-        subscription.Line,
-        subscription.SubscriptionRecomputePriceStart,
-        module='sale_subscription_recompute_price', type_='model')
-    Pool.register(
-        subscription.SubscriptionRecomputePrice,
-        module='sale_subscription_recompute_price', type_='wizard')
